002d5b6a0dfda6af99935afaaecaa454a62b32ee
DrDisagree <crazymahmud08@gmail.com>
2024-11-24 18:52:31 +0600

    Improve countdown for different device size

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -4,22 +4,22 @@ const Timer = ({ time }: { time: number }) => {
   const timeLeft = getFormattedTime(time);
 
   return (
-    <div className="grid w-full grid-cols-3 flex-row justify-center lg:justify-start mt-10 gap-3 lg:gap-8">
-      <span className="flex flex-col flex-1 flex-grow justify-center items-center bg-black text-yellow text-5xl lg:text-9xl min-w-28 lg:min-w-64 px-6 pt-3 pb-5 shadow-lg rounded-3xl">
+    <div className="grid w-full grid-cols-3 flex-row justify-center lg:justify-start mt-10 gap-3 md:gap-5 lg:gap-8 px-4 lg:px-12">
+      <span className="flex flex-col flex-1 flex-grow justify-center items-center bg-black text-yellow text-5xl sm:text-7xl md:text-9xl lg:text-[10rem] xl:text-[12rem] min-w-28 sm:min-w-40 md:min-w-48 lg:min-w-56 xl:min-w-64 px-6 pt-3 pb-5 shadow-lg rounded-3xl md:rounded-[2.5rem] xl:rounded-[3.5rem]">
         {timeLeft.hours}
-        <small className="text-xs lg:text-lg uppercase font-medium text-white">
+        <small className="text-xs md:text-lg xl:text-xl uppercase font-medium text-white">
           Hours
         </small>
       </span>
-      <span className="flex flex-col flex-1 flex-grow justify-center items-center bg-black text-yellow text-5xl lg:text-9xl min-w-28 lg:min-w-64 px-6 pt-3 pb-5 shadow-lg rounded-3xl">
+      <span className="flex flex-col flex-1 flex-grow justify-center items-center bg-black text-yellow text-5xl sm:text-7xl md:text-9xl lg:text-[10rem] xl:text-[12rem] min-w-28 sm:min-w-40 md:min-w-48 lg:min-w-56 xl:min-w-64 px-6 pt-3 pb-5 shadow-lg rounded-3xl md:rounded-[2.5rem] xl:rounded-[3.5rem]">
         {timeLeft.minutes}
-        <small className="text-xs lg:text-lg uppercase font-medium text-white">
+        <small className="text-xs md:text-lg xl:text-xl uppercase font-medium text-white">
           Minutes
         </small>
       </span>
-      <span className="flex flex-col flex-1 flex-grow justify-center items-center bg-black text-yellow text-5xl lg:text-9xl min-w-28 lg:min-w-64 px-6 pt-3 pb-5 shadow-lg rounded-3xl">
+      <span className="flex flex-col flex-1 flex-grow justify-center items-center bg-black text-yellow text-5xl sm:text-7xl md:text-9xl lg:text-[10rem] xl:text-[12rem] min-w-28 sm:min-w-40 md:min-w-48 lg:min-w-56 xl:min-w-64 px-6 pt-3 pb-5 shadow-lg rounded-3xl md:rounded-[2.5rem] xl:rounded-[3.5rem]">
         {timeLeft.seconds}
-        <small className="text-xs lg:text-lg uppercase font-medium text-white">
+        <small className="text-xs md:text-lg xl:text-xl uppercase font-medium text-white">
           Seconds
         </small>
       </span>
