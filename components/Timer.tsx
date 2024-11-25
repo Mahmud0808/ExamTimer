70ef207f0a815508f0e4ea5081af626884ed27d7
DrDisagree <crazymahmud08@gmail.com>
2024-11-25 14:47:36 +0600

    Make timer boxes little bit taller

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -5,19 +5,19 @@ const Timer = ({ time }: { time: number }) => {
 
   return (
     <div className="grid w-full grid-cols-3 flex-row justify-center lg:justify-start mt-10 gap-3 md:gap-5 lg:gap-8 px-4 lg:px-12">
-      <span className="flex flex-col flex-1 flex-grow justify-center items-center bg-black text-yellow text-5xl sm:text-7xl md:text-9xl lg:text-[10rem] xl:text-[12rem] min-w-28 sm:min-w-40 md:min-w-52 lg:min-w-64 xl:min-w-80 px-6 pt-3 pb-5 shadow-lg rounded-3xl md:rounded-[2.5rem] xl:rounded-[3.5rem] select-none">
+      <span className="flex flex-col flex-1 flex-grow justify-center items-center bg-black text-yellow text-5xl sm:text-7xl md:text-9xl lg:text-[10rem] xl:text-[12rem] min-w-28 sm:min-w-40 md:min-w-52 lg:min-w-64 xl:min-w-80 px-6 pt-3 pb-5 md:pt-4 md:pb-6 lg:pt-5 lg:pb-7 shadow-lg rounded-3xl md:rounded-[2.5rem] xl:rounded-[3.5rem] select-none">
         {timeLeft.hours}
         <small className="text-xs md:text-lg xl:text-xl uppercase font-medium text-white select-none">
           Hours
         </small>
       </span>
-      <span className="flex flex-col flex-1 flex-grow justify-center items-center bg-black text-yellow text-5xl sm:text-7xl md:text-9xl lg:text-[10rem] xl:text-[12rem] min-w-28 sm:min-w-40 md:min-w-52 lg:min-w-64 xl:min-w-80 px-6 pt-3 pb-5 shadow-lg rounded-3xl md:rounded-[2.5rem] xl:rounded-[3.5rem] select-none">
+      <span className="flex flex-col flex-1 flex-grow justify-center items-center bg-black text-yellow text-5xl sm:text-7xl md:text-9xl lg:text-[10rem] xl:text-[12rem] min-w-28 sm:min-w-40 md:min-w-52 lg:min-w-64 xl:min-w-80 px-6 pt-3 pb-5 md:pt-4 md:pb-6 lg:pt-5 lg:pb-7 shadow-lg rounded-3xl md:rounded-[2.5rem] xl:rounded-[3.5rem] select-none">
         {timeLeft.minutes}
         <small className="text-xs md:text-lg xl:text-xl uppercase font-medium text-white select-none">
           Minutes
         </small>
       </span>
-      <span className="flex flex-col flex-1 flex-grow justify-center items-center bg-black text-yellow text-5xl sm:text-7xl md:text-9xl lg:text-[10rem] xl:text-[12rem] min-w-28 sm:min-w-40 md:min-w-52 lg:min-w-64 xl:min-w-80 px-6 pt-3 pb-5 shadow-lg rounded-3xl md:rounded-[2.5rem] xl:rounded-[3.5rem] select-none">
+      <span className="flex flex-col flex-1 flex-grow justify-center items-center bg-black text-yellow text-5xl sm:text-7xl md:text-9xl lg:text-[10rem] xl:text-[12rem] min-w-28 sm:min-w-40 md:min-w-52 lg:min-w-64 xl:min-w-80 px-6 pt-3 pb-5 md:pt-4 md:pb-6 lg:pt-5 lg:pb-7 shadow-lg rounded-3xl md:rounded-[2.5rem] xl:rounded-[3.5rem] select-none">
         {timeLeft.seconds}
         <small className="text-xs md:text-lg xl:text-xl uppercase font-medium text-white select-none">
           Seconds
